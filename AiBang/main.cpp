22f6c6c08553237ab743babd394fd89adc29698f
LinRayx <443999592@qq.com>
2019-10-30 11:40:04 +0800

    Initial Commit

---
+++ AiBang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AiBang
+//
+//  Created by Linkyrie on 2019/10/30.
+//  Copyright © 2019 林恺锐. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
