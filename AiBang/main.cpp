bfe953747fa6eeb04f04f2a886472ac8a35cb7c7
LinRayx <443999592@qq.com>
2019-11-01 16:49:21 +0800

    ai

--- AiBang/main.cpp
+++ AiBang/main.cpp
@@ -6,10 +6,16 @@
 //  Copyright © 2019 林恺锐. All rights reserved.
 //
 
+#include "aigobang.hpp"
+#include <memory>
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main() {
+    std::shared_ptr<AiGoBang> p = std::make_shared<AiGoBang>();
+//    p->setPos(7, 7, 1);
+//    p->setPos(3, 3, 2);
+//    cout << p->Evaluate(1) << endl;
+//    cout << p->Evaluate(2) << endl;
+    p->test();
     return 0;
 }
